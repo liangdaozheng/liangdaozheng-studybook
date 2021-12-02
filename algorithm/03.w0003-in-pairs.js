25407cb14d3fe317d60e82b571d7cecf0ed276b0
liangdaozheng <l_daozheng@163.com>
2021-12-02 11:20:59 +0800

    Update 03.w0003-in-pairs.js
    重置

--- algorithm/03.w0003-in-pairs.js
+++ algorithm/03.w0003-in-pairs.js
@@ -3,14 +3,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var swapPairs = function(head) {
-  if (head === null|| head.next === null) {
-      return head;
-  }
-  const newHead = head.next;
-  head.next = swapPairs(newHead.next);
-  newHead.next = head;
-  return newHead;
-};
+//  var swapPairs = function(head) {
+//   if (head === null|| head.next === null) {
+//       return head;
+//   }
+//   const newHead = head.next;
+//   head.next = swapPairs(newHead.next);
+//   newHead.next = head;
+//   return newHead;
+// };
 
-console.log(swapPairs([2,3,4,6]))
+// console.log(swapPairs([2,3,4,6]))
