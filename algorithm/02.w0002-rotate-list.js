911677807a11674130452cf1545f3379f1270ee9
liangdaozheng <l_daozheng@163.com>
2021-12-02 11:20:39 +0800

    Update 02.w0002-rotate-list.js
    重置

--- algorithm/02.w0002-rotate-list.js
+++ algorithm/02.w0002-rotate-list.js
@@ -3,32 +3,32 @@
  * @param {number} k
  * @return {ListNode}
  */
- var rotateRight = function(head, k) {
-  if (k === 0 || !head || !head.next) {
-      return head;
-  }
-  let n = 1;
-  let cur = head;
-  while (cur.next) {
-      cur = cur.next;
-      n++;
-  }
+//  var rotateRight = function(head, k) {
+//   if (k === 0 || !head || !head.next) {
+//       return head;
+//   }
+//   let n = 1;
+//   let cur = head;
+//   while (cur.next) {
+//       cur = cur.next;
+//       n++;
+//   }
 
-  let add = n - k % n;
-  if (add === n) {
-      return head;
-  }
+//   let add = n - k % n;
+//   if (add === n) {
+//       return head;
+//   }
 
-  cur.next = head;
-  while (add) {
-      cur = cur.next;
-      add--;
-  }
+//   cur.next = head;
+//   while (add) {
+//       cur = cur.next;
+//       add--;
+//   }
 
-  const ret = cur.next;
-  cur.next = null;
-  return ret;
-};
+//   const ret = cur.next;
+//   cur.next = null;
+//   return ret;
+// };
 
 
-console.log(rotateRight([1,2,2,3,4],3))
+// console.log(rotateRight([1,2,2,3,4],3))
